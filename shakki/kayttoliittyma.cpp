4d3ddcbbb0bd458978b78aa1fcba6cc737fe995c
Tomas Valkendorff <Tomvalken1@gmail.com>
2021-01-12 13:22:08 +0200

    noni

---
+++ shakki/kayttoliittyma.cpp
@@ -0,0 +1,44 @@
+#include <Windows.h>
+#include <iostream>
+#include <string>
+#include <fcntl.h>
+#include <io.h>
+#include <iostream>
+#include "kayttoliittyma.h"
+
+using namespace std;
+
+
+Kayttoliittyma* Kayttoliittyma::instance = 0;
+
+
+Kayttoliittyma* Kayttoliittyma::getInstance()
+{
+	if (instance == 0)
+		instance = new Kayttoliittyma();
+	return instance;
+}
+
+
+void Kayttoliittyma::piirraLauta()
+{
+}
+
+
+/*
+	Aliohjelma tarkistaa että käyttäjän antama syöte siirroksi on 
+	muodollisesti korrekti (ei tarkista aseman laillisuutta)
+	Ottaa irti myös nappulan kirjaimen (K/D/L/R/T), tarkistaa että kirjain korrekti
+*/
+Siirto Kayttoliittyma::annaVastustajanSiirto()
+{
+	Siirto siirto;
+	return siirto;
+	
+}
+
+
+int Kayttoliittyma::kysyVastustajanVari()
+{
+	return 0;
+}
