1abad37160cfe3ae21b449ab66d12307fd2fa441
HawkOfCinders <39711821+HawkOfCinders@users.noreply.github.com>
2021-01-12 15:41:12 +0200

    Board color added

--- shakki/kayttoliittyma.cpp
+++ shakki/kayttoliittyma.cpp
@@ -25,24 +25,27 @@ void Kayttoliittyma::piirraLauta()
 	_setmode(_fileno(stdout), _O_U16TEXT);
 	
 	bool lastSquareBlack = true;
+
+	
 	for (int i = 0; i < 8; i++)
 	{
 		for (int j = 0; j < 8; j++)
 		{
 			if(lastSquareBlack){
-				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BACKGROUND_INTENSITY | BACKGROUND_RED |
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BACKGROUND_INTENSITY | BACKGROUND_RED  |
 					BACKGROUND_GREEN | BACKGROUND_BLUE);
 			}
-			else SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BACKGROUND_INTENSITY);
+			else SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BACKGROUND_GREEN);
 
 			if (_asema->_lauta[i][j] == nullptr) {
 				wcout << "   ";
 			}
 			else wcout <<" " << _asema->_lauta[i][j]->getUnicode()<<" ";
 
-			lastSquareBlack != lastSquareBlack;
+			lastSquareBlack = !lastSquareBlack;
 		}
 		wcout << "\n";
+		lastSquareBlack = !lastSquareBlack;
 	}
 }
 
