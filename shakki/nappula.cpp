9979dbb10e32890da43a45ca9a5ea57ff875594c
HawkOfCinders <39711821+HawkOfCinders@users.noreply.github.com>
2021-01-21 11:23:34 +0200

    Torni moves added to nappula.cpp

--- shakki/nappula.cpp
+++ shakki/nappula.cpp
@@ -15,6 +15,42 @@ Nappula::Nappula(wstring unicode, int vari, int koodi)
 
 void Torni::annaSiirrot(std::list<Siirto>& lista, Ruutu* ruutu, Asema* asema, int vari)
 {
+	int aloitusRivi = ruutu->getRivi();
+	int aloitusSarake = ruutu->getSarake();
+	for (int i = ruutu->getRivi(); i < 8; i++){
+		if (asema->_lauta[i][aloitusSarake] == nullptr)
+			lista.push_back(Siirto(Ruutu(aloitusRivi, aloitusSarake), Ruutu(i, aloitusSarake)));
+		else if (asema->_lauta[i][aloitusRivi]->getVari() != vari) {
+			lista.push_back(Siirto(Ruutu(aloitusRivi, aloitusSarake), Ruutu(i, aloitusSarake)));
+		}	
+		else break;
+	}
+	for (int i = ruutu->getRivi(); i >= 0; i--) {
+		if (asema->_lauta[i][aloitusSarake] == nullptr)
+			lista.push_back(Siirto(Ruutu(aloitusRivi, aloitusSarake), Ruutu(i, aloitusSarake)));
+		else if (asema->_lauta[i][aloitusRivi]->getVari() != vari) {
+			lista.push_back(Siirto(Ruutu(aloitusRivi, aloitusSarake), Ruutu(i, aloitusSarake)));
+		}
+		else break;
+	}
+	for (int i = ruutu->getSarake(); i < 8; i++) {
+		if (asema->_lauta[aloitusRivi][i] == nullptr)
+			lista.push_back(Siirto(Ruutu(aloitusRivi, aloitusSarake), Ruutu(aloitusRivi, i)));
+		else if (asema->_lauta[i][aloitusRivi]->getVari() != vari) {
+			lista.push_back(Siirto(Ruutu(aloitusRivi, aloitusSarake), Ruutu(aloitusRivi, i)));
+			break;
+		}
+		else break;
+	}
+	for (int i = ruutu->getSarake(); i >= 0; i--) {
+		if (asema->_lauta[aloitusRivi][i] == nullptr)
+			lista.push_back(Siirto(Ruutu(aloitusRivi, aloitusSarake), Ruutu(aloitusRivi, i)));
+		else if (asema->_lauta[aloitusRivi][i]->getVari() != vari) {
+			lista.push_back(Siirto(Ruutu(aloitusRivi, aloitusSarake), Ruutu(aloitusRivi, i)));
+			break;
+		}
+		else break;
+	}
 	
 }
 
