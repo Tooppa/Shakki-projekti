915d8a58ed90956466d2bf2695b777f5b29d0afe
HawkOfCinders <39711821+HawkOfCinders@users.noreply.github.com>
2021-03-01 16:39:48 +0200

    Changed two evaluation tables

--- shakki/asema.h
+++ shakki/asema.h
@@ -46,6 +46,7 @@ public:
 	bool getOnkoValkeaKTliikkunut();						// Linnoittuminen mahdollista?
 	bool getOnkoMustaDTliikkunut();							// Linnoittuminen mahdollista?
 	bool getOnkoMustaKTliikkunut();							// Linnoittuminen mahdollista?
+
 	
 private:
 
